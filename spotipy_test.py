8243584c97bd7a4a2dcaa51b94d705f203b0c6ef
Jack Trowbridge <jytrowbridge@gmail.com>
2020-10-02 17:17:14 -0400

    Spotify skeleton

--- spotipy_test.py
+++ spotipy_test.py
@@ -1,25 +1,35 @@
-
+import json
+from spotipy.oauth2 import SpotifyClientCredentials
 import spotipy
 import spotipy.util as util
-# from spotipy.oauth2 import SpotifyOAuth
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
+CLIENT_ID = os.getenv('SPOTIFY_CLIENT_ID')
+CLIENT_SECRET = os.getenv('SPOTIFY_CLIENT_SECRET')
 
-CLIENT_ID='4077f916fc784360a6d482594ca43713'
-CLIENT_SECRET='de192af6886f453784c692f2791968ae'
-
-scope = "user-library-read"
+scope = "playlist-modify-private playlist-modify-public user-library-modify"
 username = "1247468717"
 redirect_uri = "http://localhost:8888/callback/"
 
-token = spotipy.util.prompt_for_user_token(username, scope, CLIENT_ID, CLIENT_SECRET, redirect_uri)
-sp = spotipy.Spotify(auth=token)
+token = util.prompt_for_user_token(username, scope, CLIENT_ID, CLIENT_SECRET, redirect_uri)
+sp = spotipy.Spotify(auth=token,
+                     client_credentials_manager=SpotifyClientCredentials(
+                         client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
+                     )
 
-# token = spotipy.util.oauth2.SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
+# sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET))
 
-# cache_token = token.get_access_token()
-# spotify = spotipy.Spotify(cache_token)
+# # sp.user_playlist_create(username, 'Test SPOTIPY playlist 2')
 
-results = sp.current_user_saved_tracks()
-for idx, item in enumerate(results['items']):
-    track = item['track']
-    print(idx, track['artists'][0]['name'], " – ", track['name'])
+
+name = 'chopped in half'
+artist = 'obituary'
+
+results = sp.search(q=f'track:{ name} artist:{artist}', type='track', limit=1)
+print(json.dumps(results, indent=2))
+print(results['tracks']['items'][0]['album']['id'])
+
+#current_user_saved_tracks_add(
+sp.current_user_saved_albums_add(['79fVRZLnIqS3FytTLfTBT4'])

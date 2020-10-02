a4d7fa4ce26a34c6746711fc9f5182f64c424d7a
Jack <jytrowbridge@gmail.com>
2020-10-02 13:40:37 -0400

    Update spotipy skeleton

--- spotipy_test.py
+++ spotipy_test.py
@@ -1,9 +1,23 @@
+
 import spotipy
-from spotipy.oauth2 import SpotifyOAuth
+import spotipy.util as util
+# from spotipy.oauth2 import SpotifyOAuth
+
+
+CLIENT_ID='4077f916fc784360a6d482594ca43713'
+CLIENT_SECRET='de192af6886f453784c692f2791968ae'
 
 scope = "user-library-read"
+username = "1247468717"
+redirect_uri = "http://localhost:8888/callback/"
 
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+token = spotipy.util.prompt_for_user_token(username, scope, CLIENT_ID, CLIENT_SECRET, redirect_uri)
+sp = spotipy.Spotify(auth=token)
+
+# token = spotipy.util.oauth2.SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
+
+# cache_token = token.get_access_token()
+# spotify = spotipy.Spotify(cache_token)
 
 results = sp.current_user_saved_tracks()
 for idx, item in enumerate(results['items']):
